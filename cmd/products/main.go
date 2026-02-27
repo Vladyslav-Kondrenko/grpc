acb472da994d386a8a81640776a2fe38a7d21d68
Vladyslav Pobihailo <vladyslav.pobihailo@boosta.co>
2026-02-27 20:46:47 +0100

    ✨ add Docker setup with PostgreSQL and API service

--- cmd/products/main.go
+++ cmd/products/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Vladyslav-Kondrenko/grpc.git/internal/app/products/handler"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, products!")
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	router := gin.Default()
+	router.POST("/products", handler.CreateProduct)
+	router.PUT("/products/:id", handler.UpdateProduct)
+	router.DELETE("/products/:id", handler.DeleteProduct)
+	router.GET("/products", handler.GetAllProducts)
+	router.Run(":8080")
+
 }
